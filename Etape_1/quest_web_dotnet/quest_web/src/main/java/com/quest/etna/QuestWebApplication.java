b5d7c26a71c65c02d4cd226ac36910cd1f3b0a75
Amine372 <nakhila@orange.fr>
2024-01-10 21:12:57 +0100

    Update QuestWebApplication.java

--- Etape_1/quest_web_dotnet/quest_web/src/main/java/com/quest/etna/QuestWebApplication.java
+++ Etape_1/quest_web_dotnet/quest_web/src/main/java/com/quest/etna/QuestWebApplication.java
@@ -1,4 +1,5 @@
-package com.examplecom.quest.quest_web;
+package com.quest.etna;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
