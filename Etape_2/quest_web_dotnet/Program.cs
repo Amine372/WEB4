28efc0a387ddd6c94b19750af62e02d1c3f0125d
Amine372 <nakhila@orange.fr>
2024-01-11 23:16:47 +0100

    Update Program.cs

--- Etape_2/quest_web_dotnet/Program.cs
+++ Etape_2/quest_web_dotnet/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Configuration de Entity Framework Core avec MySQL
+builder.Services.AddDbContext<APIDbContext>(options =>
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
